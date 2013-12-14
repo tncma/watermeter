902d5edfb77f3bec1dd1f5079bd76f5cf950ec46
Fizer Khan <fizerkhan@gmail.com>
2013-12-14 15:17:39 +0530

    Initial Commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,9 @@
+/*global define */
+//
+// It holds main functionality of the application.
+//
+define(['waterbodies', 'watermeter'], function (waterbodies, watermeter) {
+    'use strict';
+
+    return 'We are going to rock';
+});
---
+++ app/scripts/main.js
@@ -0,0 +1,76 @@
+//
+// This is where our application start its engine.
+//
+
+// Require JS configuration
+require.config({
+    // Initialize the application with the main application file.
+    deps: ['main'],
+
+    paths: {
+
+        // Third party plugins
+        jquery: '../bower_components/jquery/jquery',
+        bootstrapAffix: '../bower_components/sass-bootstrap/js/affix',
+        bootstrapAlert: '../bower_components/sass-bootstrap/js/alert',
+        bootstrapButton: '../bower_components/sass-bootstrap/js/button',
+        bootstrapCarousel: '../bower_components/sass-bootstrap/js/carousel',
+        bootstrapCollapse: '../bower_components/sass-bootstrap/js/collapse',
+        bootstrapDropdown: '../bower_components/sass-bootstrap/js/dropdown',
+        bootstrapModal: '../bower_components/sass-bootstrap/js/modal',
+        bootstrapPopover: '../bower_components/sass-bootstrap/js/popover',
+        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/scrollspy',
+        bootstrapTab: '../bower_components/sass-bootstrap/js/tab',
+        bootstrapTooltip: '../bower_components/sass-bootstrap/js/tooltip',
+        bootstrapTransition: '../bower_components/sass-bootstrap/js/transition',
+    },
+    shim: {
+        bootstrapAffix: {
+            deps: ['jquery']
+        },
+        bootstrapAlert: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapButton: {
+            deps: ['jquery']
+        },
+        bootstrapCarousel: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapCollapse: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapDropdown: {
+            deps: ['jquery']
+        },
+        bootstrapModal:{
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapPopover: {
+            deps: ['jquery', 'bootstrapTooltip']
+        },
+        bootstrapScrollspy: {
+            deps: ['jquery']
+        },
+        bootstrapTab: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapTooltip: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapTransition: {
+            deps: ['jquery']
+        }
+    }
+});
+
+// Starting point of the application
+require(['jquery', 'app' 'bootstrapCollapse',
+    'bootstrapTransition'], function ($, app) {
+    'use strict';
+
+    $(function () {
+
+    });
+
+});
